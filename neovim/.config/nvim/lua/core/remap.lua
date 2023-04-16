d406852a3e5592b46cd820cd83fe49b527611b1a
Rizqirazkafi <rizqirazkafi56@gmail.com>
2023-04-16 15:35:13 +0700

    Added remap for window management and terminal

--- neovim/.config/nvim/lua/core/remap.lua
+++ neovim/.config/nvim/lua/core/remap.lua
@@ -8,3 +8,15 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("x", "<leader>p", [["_dP]])
+-- Remap split navigation
+vim.keymap.set('n', '<C-h>', '<C-w>h')
+vim.keymap.set('n', '<C-j>', '<C-w>j')
+vim.keymap.set('n', '<C-k>', '<C-w>k')
+vim.keymap.set('n', '<C-l>', '<C-w>l')
+-- Adjust split sizes
+vim.keymap.set('n', '<left>', '<cmd>vertical res +3<CR>')
+vim.keymap.set('n', '<right>', '<cmd>vertical res -3<CR>')
+vim.keymap.set('n', '<up>', '<cmd>res +3<CR>')
+vim.keymap.set('n', '<down>', '<cmd>res -3<CR>')
+-- Remap for exiting Nvim terminal
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
